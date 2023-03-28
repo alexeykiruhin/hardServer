52ae94d1dc4872e35755841b440538a0f26ed255
Alexey Kiruhin <58473405+alexeykiruhin@users.noreply.github.com>
2023-03-28 19:05:19 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import json
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code
+    list = {{'id': 1, 'name': 'PLayer1'}, {'id': 2, 'name': 'PLayer2'}}
+    msg_json = json.dumps(list)
+    return msg_json
+
+
+if __name__ == '__main__':
+    app.run()
