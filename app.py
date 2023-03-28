e823d71ff100d3718319496b56970ff9947f17bd
alexeykiryukhin <q23sa3501080@gmail.com>
2023-03-29 00:28:29 +0300

    add updates

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask
+from flask_cors import CORS
 import json
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
 
 @app.route('/')
-def hello_world():  # put application's code
-    list = {{'id': 1, 'name': 'PLayer1'}, {'id': 2, 'name': 'PLayer2'}}
-    msg_json = json.dumps(list)
+def getusers():  # put application's code
+    users_list = [{'id': 1, 'name': 'PLayer1'}, {'id': 2, 'name': 'PLayer2'}]
+    users = {'users': users_list, 'totalCount': len(users_list)}
+    msg_json = json.dumps(users)
     return msg_json
 
 
