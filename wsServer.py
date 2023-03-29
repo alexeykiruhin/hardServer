c82866d159ee58ca878918cfd09f954c094d7269
alexeykiruhin <q23sa3501080@gmail.com>
2023-03-29 17:57:35 +0300

    add ws server

---
+++ wsServer.py
@@ -0,0 +1,16 @@
+import asyncio
+import websockets
+
+async def websocket_handler(websocket, path):
+    async for message in websocket:
+        # Обработка входящих сообщений от клиента
+        response = f"Вы сказали: {message}"
+        await websocket.send(response)
+
+async def main():
+    async with websockets.serve(websocket_handler, "localhost", 8765):
+        print('awaiting...')
+        await asyncio.Future()  # Ожидание завершения работы сервера
+
+if __name__ == "__main__":
+    asyncio.run(main())
