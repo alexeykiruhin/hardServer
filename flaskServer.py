8bb03cd0ca242b90582267337d869cf5a80013c7
alexeykiryukhin <q23sa3501080@gmail.com>
2023-04-05 00:15:36 +0300

    обновил сервер для выдачи постов по 2 шт, для тренировки пагинации на фронте

--- flaskServer.py
+++ flaskServer.py
@@ -1,31 +1,23 @@
-import json
-
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
-from flask_cors import CORS
+from flask import Flask, jsonify
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, cors_allowed_origins='*')
 
-CORS(app)
-
-
-@app.route('/')
-def index():
-    return json.dumps([{'id': 1, 'name': 'PLayer1'}, {'id': 2, 'name': 'PLayer2'}])
-
-
-@socketio.on('connect')
-def test_connect():
-    print('Client connected')
-    emit('my response', {'data': 'Connected'})
+posts = [
+    {"id": 1, "nameUser": "user1", "textPost": "Textetxetetdteksdgd"},
+    {"id": 2, "nameUser": "user2", "textPost": "Textetasdasdxetetdteksdgd"},
+    {"id": 3, "nameUser": "user3", "textPost": "Textetasdas123dteksdgd"},
+    {"id": 4, "nameUser": "user4", "textPost": "Textetasdasdxetetdteksdgd"},
+    {"id": 5, "nameUser": "user5", "textPost": "Textetxetetdteksdgd"},
+]
 
 
-@socketio.on('disconnect')
-def test_disconnect():
-    print('Client disconnected')
+@app.route('/posts/<int:start>')
+def get_posts(start):
+    end = start + 2
+    response = jsonify({'posts': posts[start:end]})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 
 if __name__ == '__main__':
-    socketio.run(app)
+    app.run()
