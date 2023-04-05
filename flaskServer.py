e10435c04f9b06915e0631b9e00201e92bfdd09d
alexeykiryukhin <q23sa3501080@gmail.com>
2023-04-05 11:28:22 +0300

    добавил обработку ошибки при передаче гет параметра page = 1

--- flaskServer.py
+++ flaskServer.py
@@ -1,8 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/posts": {"origins": "*"}})
 
 posts = [
+    {"id": 0, "nameUser": "user0", "textPost": "Textetxetetdteksdgd"},
     {"id": 1, "nameUser": "user1", "textPost": "Textetxetetdteksdgd"},
     {"id": 2, "nameUser": "user2", "textPost": "Textetasdasdxetetdteksdgd"},
     {"id": 3, "nameUser": "user3", "textPost": "Textetasdas123dteksdgd"},
@@ -11,11 +14,25 @@ posts = [
 ]
 
 
-@app.route('/posts/<int:start>')
-def get_posts(start):
-    end = start + 2
-    response = jsonify({'posts': posts[start:end]})
+@app.route('/posts', methods=['GET', 'OPTIONS'])
+def get_posts():
+    print(request.args.get('page'))
+    page_number = int(request.args.get('page', 1))
+    # бработка ошибки передачи параметра page=1
+    if page_number == 0:
+        page_number = 1
+    page_size = 2
+    start = (page_number - 1) * page_size
+    end = start + page_size
+    response = jsonify({'posts': posts[start:end], 'count': len(posts)})
+    return response
+
+
+@app.after_request
+def add_cors_headers(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
     return response
 
 
