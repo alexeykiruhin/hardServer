020dc39088c6b7ae8bd70b4617a616a20ab355bc
alexeykiryukhin <q23sa3501080@gmail.com>
2023-04-06 23:36:47 +0300

    Добавил обработку регистрации и верификации

--- flaskServer.py
+++ flaskServer.py
@@ -1,17 +1,143 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user, login_required
 
 app = Flask(__name__)
 CORS(app, resources={r"/posts": {"origins": "*"}})
 
+# имитация базы данных пользователей
+users = {
+    1: {'id': 1, 'username': 'Alice', 'password': 'password1', 'img': 'https://randomuser.me/api/portraits/women/69.jpg'},
+    2: {'id': 2, 'username': 'Dave', 'password': 'password1', 'img': 'https://randomuser.me/api/portraits/women/74.jpg'},
+    3: {'id': 3, 'username': 'Bob', 'password': 'password1', 'img': 'https://randomuser.me/api/portraits/men/11.jpg'},
+    4: {'id': 4, 'username': 'Ivan', 'password': 'password1', 'img': 'https://randomuser.me/api/portraits/women/55.jpg'},
+    5: {'id': 5, 'username': 'user5', 'password': 'password1', 'img': 'https://randomuser.me/api/portraits/women/20.jpg'},
+    6: {'id': 6, 'username': 'user6', 'password': 'password1', 'img': 'https://randomuser.me/api/portraits/women/84.jpg'},
+    7: {'id': 7, 'username': 'user7', 'password': 'password1', 'img': 'https://randomuser.me/api/portraits/men/84.jpg'},
+    8: {'id': 8, 'username': 'user8', 'password': 'password1', 'img': 'https://randomuser.me/api/portraits/men/8.jpg'},
+    9: {'id': 9, 'username': 'user9', 'password': 'password1', 'img': 'https://randomuser.me/api/portraits/men/17.jpg'},
+    10: {'id': 10, 'username': 'user10', 'password': 'password1',
+     'img': 'https://randomuser.me/api/portraits/women/84.jpg'},
+    11: {'id': 11, 'username': 'user11', 'password': 'password1', 'img': 'https://randomuser.me/api/portraits/men/31.jpg'},
+    12: {'id': 12, 'username': 'user12', 'password': 'password1', 'img': 'https://randomuser.me/api/portraits/men/93.jpg'},
+    13: {'id': 13, 'username': 'user13', 'password': 'password1',
+     'img': 'https://randomuser.me/api/portraits/women/92.jpg'},
+    14: {'id': 14, 'username': 'user14', 'password': 'password1', 'img': 'https://randomuser.me/api/portraits/men/61.jpg'},
+    15: {'id': 15, 'username': 'user15', 'password': 'password1', 'img': 'https://randomuser.me/api/portraits/men/11.jpg'},
+    16: {'id': 16, 'username': 'user16', 'password': 'password1',
+     'img': 'https://randomuser.me/api/portraits/women/18.jpg'},
+    17: {'id': 17, 'username': 'user17', 'password': 'password1',
+     'img': 'https://randomuser.me/api/portraits/women/25.jpg'},
+    18: {'id': 18, 'username': 'user18', 'password': 'password1',
+     'img': 'https://randomuser.me/api/portraits/women/77.jpg'},
+    19: {'id': 19, 'username': 'user19', 'password': 'password1', 'img': 'https://randomuser.me/api/portraits/men/89.jpg'},
+    20: {'id': 20, 'username': 'user20', 'password': 'password1',
+     'img': 'https://randomuser.me/api/portraits/women/98.jpg'}
+}
+
 posts = [
-    {"id": 0, "nameUser": "user0", "textPost": "Textetxetetdteksdgd"},
-    {"id": 1, "nameUser": "user1", "textPost": "Textetxetetdteksdgd"},
-    {"id": 2, "nameUser": "user2", "textPost": "Textetasdasdxetetdteksdgd"},
-    {"id": 3, "nameUser": "user3", "textPost": "Textetasdas123dteksdgd"},
-    {"id": 4, "nameUser": "user4", "textPost": "Textetasdasdxetetdteksdgd"},
-    {"id": 5, "nameUser": "user5", "textPost": "Textetxetetdteksdgd"},
+    {"id": 1, "userId": users[1]['id'], "nameUser": "Alice",
+     "textPost": "Интересные у тебя татуировки на ногах. Что они означают? Это варикоз."},
+    {"id": 2, "userId": users[2]['id'], "nameUser": "Dave",
+     "textPost": "Сегодня я настолько ленив, что мои сны уже имеют субтитры."},
+    {"id": 3, "userId": users[3]['id'], "nameUser": "Bob",
+     "textPost": "Если ты думаешь, что никогда не совершал глупых поступков, то ты просто не обращал на них внимания."},
+    {"id": 4, "userId": users[4]['id'], "nameUser": "Ivan",
+     "textPost": "Женщины не стареют, они просто переходят на другой уровень!"},
+    {"id": 5, "userId": users[5]['id'], "nameUser": "user5",
+     "textPost": "Если ты думаешь, что всё уже позади, то скорее всего ты идёшь задом наперёд."},
+    {"id": 6, "userId": users[6]['id'], "nameUser": "Bob",
+     "textPost": "Если бы я был денежным знаком, то я был бы знаком вопросительным, потому что жизнь полна неожиданных поворотов."},
+    {"id": 7, "userId": users[7]['id'], "nameUser": "Julia",
+     "textPost": "Я не ленивый, я просто экономлю энергию на важные вещи."},
+    {"id": 8, "userId": users[8]['id'], "nameUser": "Dave",
+     "textPost": "Все люди делятся на две категории: те, кто могут делать математические вычисления, и те, кто не могут."},
+    {"id": 9, "userId": users[9]['id'], "nameUser": "user9",
+     "textPost": "Будущее близко, но в прошлое можно вернуться с помощью гугла."},
+    {"id": 10, "userId": users[10]['id'], "nameUser": "user10",
+     "textPost": "Я не знаю, какие планы на сегодня у других, но у меня есть важное свидание с моей постелью."},
+    {"id": 11, "userId": users[11]['id'], "nameUser": "user11",
+     "textPost": "Мне кажется, что кто-то где-то смотрит на меня и думает: «Ну вот идиот…»."},
 ]
+
+# инициализация LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+# users = {
+#     1: {'id': 1, 'username': 'user1', 'password': 'password1'},
+#     2: {'id': 2, 'username': 'user2', 'password': 'password2'},
+#     3: {'id': 3, 'username': 'user3', 'password': 'password3'}
+# }
+
+
+# реализация класса пользователя
+class User(UserMixin):
+    pass
+
+
+# реализация функции для получения пользователя по его id
+@login_manager.user_loader
+def load_user(user_id):
+    user = User()
+    user.id = int(user_id)
+    return user
+
+
+# обработка запроса на регистрацию пользователя
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.json.get('username')
+    password = request.json.get('password')
+
+    if username and password:
+        # проверяем, что такой пользователь не существует
+        for user in users.values():
+            if user['username'] == username:
+                return jsonify({'error': 'User already exists'})
+
+        # создаем нового пользователя
+        user_id = max(users.keys()) + 1
+        users[user_id] = {'id': user_id, 'username': username, 'password': password}
+        return jsonify({'message': 'User registered successfully'})
+    else:
+        return jsonify({'error': 'Missing username or password'})
+
+
+# обработка запроса на авторизацию пользователя
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.json.get('username')
+    password = request.json.get('password')
+
+    if not username or not password:
+        return jsonify({'error': 'Missing username or password'})
+
+    # проверяем, что пользователь существует и введенный пароль верный
+    for user in users.values():
+        if user['username'] == username and user['password'] == password:
+            user_obj = User()
+            user_obj.id = user['id']
+            login_user(user_obj)
+            return jsonify({'message': 'User logged in successfully'})
+
+    return jsonify({'error': 'Invalid username or password'})
+
+
+# обработка запроса на выход из системы
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return jsonify({'message': 'User logged out successfully'})
+
+
+# защита эндпоинта для авторизованных пользователей
+@app.route('/protected')
+@login_required
+def protected():
+    return jsonify({'message': 'You are authorized!'})
 
 
 @app.route('/posts', methods=['GET', 'OPTIONS'])
@@ -25,6 +151,22 @@ def get_posts():
     start = (page_number - 1) * page_size
     end = start + page_size
     response = jsonify({'posts': posts[start:end], 'count': len(posts)})
+    return response
+
+
+@app.route('/users', methods=['GET', 'OPTIONS'])
+def get_users():
+    users_list = list(users.values())
+    response = jsonify({'users': users_list, 'count': len(users)})
+    print(response)
+    return response
+
+
+@app.route('/user/<int:user_id>', methods=['GET', 'OPTIONS'])
+def get_user(user_id):
+    users_list = list(users.values())
+    response = jsonify({'user': users_list[user_id - 1]})
+    print(response)
     return response
 
 
