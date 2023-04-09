58e05166b6aaa5c14aa2f7b4cbb4110a1fd6fc90
alexeykiryukhin <q23sa3501080@gmail.com>
2023-04-09 14:38:44 +0300

    Пофиксил некоторые предупреждения, изменил функцию регистрации юзера, но не сделал добавление в бд, пока заглушка

--- flaskServer.py
+++ flaskServer.py
@@ -12,71 +12,10 @@ app = Flask(__name__)
 CORS(app, resources={r"/posts": {"origins": "*"}})
 app.secret_key = 'secret'
 
-# имитация базы данных пользователей
-# users = {
-#     1: {'id': 1, 'username': 'Alice', 'password': 'password1', 'img': 'https://randomuser.me/api/portraits/women/69.jpg'},
-#     2: {'id': 2, 'username': 'Dave', 'password': 'password1', 'img': 'https://randomuser.me/api/portraits/men/74.jpg'},
-#     3: {'id': 3, 'username': 'Bob', 'password': 'password1', 'img': 'https://randomuser.me/api/portraits/men/11.jpg'},
-#     4: {'id': 4, 'username': 'Ivan', 'password': 'password1', 'img': 'https://randomuser.me/api/portraits/men/25.jpg'},
-#     5: {'id': 5, 'username': 'user5', 'password': 'password1', 'img': 'https://randomuser.me/api/portraits/women/20.jpg'},
-#     6: {'id': 6, 'username': 'user6', 'password': 'password1', 'img': 'https://randomuser.me/api/portraits/women/84.jpg'},
-#     7: {'id': 7, 'username': 'user7', 'password': 'password1', 'img': 'https://randomuser.me/api/portraits/men/13.jpg'},
-#     8: {'id': 8, 'username': 'user8', 'password': 'password1', 'img': 'https://randomuser.me/api/portraits/men/8.jpg'},
-#     9: {'id': 9, 'username': 'user9', 'password': 'password1', 'img': 'https://randomuser.me/api/portraits/men/17.jpg'},
-#     10: {'id': 10, 'username': 'user10', 'password': 'password1',
-#      'img': 'https://randomuser.me/api/portraits/women/84.jpg'},
-#     11: {'id': 11, 'username': 'user11', 'password': 'password1', 'img': 'https://randomuser.me/api/portraits/men/31.jpg'},
-#     12: {'id': 12, 'username': 'user12', 'password': 'password1', 'img': 'https://randomuser.me/api/portraits/men/93.jpg'},
-#     13: {'id': 13, 'username': 'user13', 'password': 'password1',
-#      'img': 'https://randomuser.me/api/portraits/women/92.jpg'},
-#     14: {'id': 14, 'username': 'user14', 'password': 'password1', 'img': 'https://randomuser.me/api/portraits/men/61.jpg'},
-#     15: {'id': 15, 'username': 'user15', 'password': 'password1', 'img': 'https://randomuser.me/api/portraits/men/11.jpg'},
-#     16: {'id': 16, 'username': 'user16', 'password': 'password1',
-#      'img': 'https://randomuser.me/api/portraits/women/18.jpg'},
-#     17: {'id': 17, 'username': 'user17', 'password': 'password1',
-#      'img': 'https://randomuser.me/api/portraits/women/25.jpg'},
-#     18: {'id': 18, 'username': 'user18', 'password': 'password1',
-#      'img': 'https://randomuser.me/api/portraits/women/77.jpg'},
-#     19: {'id': 19, 'username': 'user19', 'password': 'password1', 'img': 'https://randomuser.me/api/portraits/men/89.jpg'},
-#     20: {'id': 20, 'username': 'user20', 'password': 'password1',
-#      'img': 'https://randomuser.me/api/portraits/women/98.jpg'}
-# }
-#
-# posts = [
-#     {"id": 1, "userId": users[1]['id'], "nameUser": "Alice",
-#      "textPost": "Интересные у тебя татуировки на ногах. Что они означают? Это варикоз."},
-#     {"id": 2, "userId": users[2]['id'], "nameUser": "Dave",
-#      "textPost": "Сегодня я настолько ленив, что мои сны уже имеют субтитры."},
-#     {"id": 3, "userId": users[3]['id'], "nameUser": "Bob",
-#      "textPost": "Если ты думаешь, что никогда не совершал глупых поступков, то ты просто не обращал на них внимания."},
-#     {"id": 4, "userId": users[4]['id'], "nameUser": "Ivan",
-#      "textPost": "Женщины не стареют, они просто переходят на другой уровень!"},
-#     {"id": 5, "userId": users[5]['id'], "nameUser": "user5",
-#      "textPost": "Если ты думаешь, что всё уже позади, то скорее всего ты идёшь задом наперёд."},
-#     {"id": 6, "userId": users[6]['id'], "nameUser": "Bob",
-#      "textPost": "Если бы я был денежным знаком, то я был бы знаком вопросительным, потому что жизнь полна неожиданных поворотов."},
-#     {"id": 7, "userId": users[7]['id'], "nameUser": "Julia",
-#      "textPost": "Я не ленивый, я просто экономлю энергию на важные вещи."},
-#     {"id": 8, "userId": users[8]['id'], "nameUser": "Dave",
-#      "textPost": "Все люди делятся на две категории: те, кто могут делать математические вычисления, и те, кто не могут."},
-#     {"id": 9, "userId": users[9]['id'], "nameUser": "user9",
-#      "textPost": "Будущее близко, но в прошлое можно вернуться с помощью гугла."},
-#     {"id": 10, "userId": users[10]['id'], "nameUser": "user10",
-#      "textPost": "Я не знаю, какие планы на сегодня у других, но у меня есть важное свидание с моей постелью."},
-#     {"id": 11, "userId": users[11]['id'], "nameUser": "user11",
-#      "textPost": "Мне кажется, что кто-то где-то смотрит на меня и думает: «Ну вот идиот…»."},
-# ]
 
 # инициализация LoginManager
 login_manager = LoginManager()
 login_manager.init_app(app)
-
-
-# users = {
-#     1: {'id': 1, 'username': 'user1', 'password': 'password1'},
-#     2: {'id': 2, 'username': 'user2', 'password': 'password2'},
-#     3: {'id': 3, 'username': 'user3', 'password': 'password3'}
-# }
 
 
 # реализация класса пользователя
@@ -114,24 +53,25 @@ def load_user(user_id):
     return user
 
 
-# обработка запроса на регистрацию пользователя
+# обработка запроса на регистрацию пользователя !не доделана
 @app.route('/register', methods=['POST'])
 def register():
-    username = request.json.get('username')
-    password = request.json.get('password')
+    # получаем данные из запроса
+    data = request.json
 
-    if username and password:
-        # проверяем, что такой пользователь не существует
-        for user in users.values():
-            if user['username'] == username:
-                return jsonify({'error': 'User already exists'})
-
-        # создаем нового пользователя
-        user_id = max(users.keys()) + 1
-        users[user_id] = {'id': user_id, 'username': username, 'password': password}
-        return jsonify({'message': 'User registered successfully'})
+    # проверяем, что такой пользователь не существует
+    user = users_collection.find_one({'username': data['username']}, {'_id': 0})
+    if user:
+        # существует
+        return jsonify({'error': 'User already exists'})
     else:
-        return jsonify({'error': 'Missing username or password'})
+        # создаем нового пользователя
+        if data['username'] and data['password']:
+            #  проверяем что логин и пароль передали
+            pass
+            return jsonify({'message': 'User registered successfully'})
+        else:
+            return jsonify({'error': 'Missing username or password'})
 
 
 # обработка запроса на авторизацию пользователя
