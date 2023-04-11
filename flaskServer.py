e04e8290f1742618f589f84c8b4ba22169230c0d
alexeykiryukhin <q23sa3501080@gmail.com>
2023-04-11 23:27:26 +0300

    Подготовка к использованию класса User и LoginManager

--- flaskServer.py
+++ flaskServer.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token, JWTManager, jwt_required, get_jwt_identity
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
 import mongo
@@ -11,7 +12,9 @@ posts_collection = mongo.posts
 app = Flask(__name__)
 CORS(app, resources={r"/posts": {"origins": "*"}})
 app.secret_key = 'secret'
+app.config['JWT_SECRET_KEY'] = '23sa3501080X'  # задаем секретный ключ для подписи токена
 
+jwt = JWTManager(app)  # инициализируем объект JWTManager
 
 # инициализация LoginManager
 login_manager = LoginManager()
@@ -20,14 +23,16 @@ login_manager.init_app(app)
 
 # реализация класса пользователя
 class User(UserMixin):
-    def __init__(self, id, username, password, img):
+    def __init__(self, id, username, password, img, rating):
         self.id = id
         self.username = username
+        self.password = password
         self.img = img
-        self.password_hash = generate_password_hash(password)
+        self.rating = rating
 
     def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+        # return check_password_hash(self.password_hash, password)
+        return self.password == password
 
     def is_authenticated(self):
         return True
@@ -44,13 +49,21 @@ class User(UserMixin):
     def get_info(self):
         return {'username': self.username, 'img': self.img}
 
+    @staticmethod
+    def authenticate(username, password):
+        user = users_collection.find_one({'username': username}, {'_id': False})
+        if user and user['password'] == password:
+            return User(user['id'], user['username'], user['password'], user['img'], user['rating'])
+        return None
+
 
 # реализация функции для получения пользователя по его id
 @login_manager.user_loader
 def load_user(user_id):
-    user = User()
-    user.id = int(user_id)
-    return user
+    user = users_collection.find_one({'id': int(user_id)}, {'_id': False})
+    if user:
+        return User(user['id'], user['username'], user['password'], user['img'], user['rating'])
+    return None
 
 
 # обработка запроса на регистрацию пользователя !не доделана
@@ -79,17 +92,23 @@ def register():
 def login():
     # получаем данные из запроса
     data = request.json
+    # print(data)
     # ищем пользователя в базе данных
     user = users_collection.find_one({'username': data['username']}, {'_id': 0})
+    # print(user)
     # проверяем пароль
     if user['password'] == data['password']:
+        print('in')
         # создаем токен
-        # token = create_access_token(identity=user['id'])
+        token = create_access_token(identity=user['id'])
+        print(token)
         # кастылём удаляю пароль из ответа
         del user['password']
         # возвращаем токен
         # return jsonify({'access_token': token})
-        return jsonify({'user_obj': user, 'isAuth': True})
+        response = jsonify({'user_obj': user, 'isAuth': True})
+        response.set_cookie('access-token', token, httponly=True)
+        return response
 
     # возвращаем ошибку
     return jsonify({'message': 'Invalid username or password'}), 401
@@ -200,7 +219,12 @@ def get_users():
 
 
 @app.route('/user/<int:user_id>', methods=['GET', 'OPTIONS'])
+# @jwt_required()  # использование декоратора для проверки токена
 def get_user(user_id):
+    # получение идентификатора пользователя из токена
+    # current_user_id = get_jwt_identity()
+    # print(current_user_id)
+
     user_info = users_collection.find_one({'id': user_id}, {'_id': 0, 'password': 0})
     user_posts = posts_collection.aggregate([
         {
@@ -242,6 +266,7 @@ def add_cors_headers(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    response.headers.add('Access-Control-Allow-Credentials', 'true')
     return response
 
 
