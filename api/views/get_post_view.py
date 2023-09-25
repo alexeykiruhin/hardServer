bc3a98afb72e2284792b505d3f4d8e75b5e09dde
alexeykiruhin <q23sa3501080@gmail.com>
2023-09-25 22:48:26 +0300

    Редактирование поста

--- api/views/get_post_view.py
+++ api/views/get_post_view.py
@@ -1,4 +1,5 @@
 # получение юзера
+from bson import ObjectId
 from flask import Blueprint, request
 from flask_jwt_extended import get_jwt_identity, jwt_required
 from mongo import posts_collection, users_collection  # переменные из файла mongo.py
@@ -10,7 +11,7 @@ api_get_post_view = Blueprint('api_get_post_view', __name__)
 # @jwt_required()  # использование декоратора для проверки токена
 def get_post_view(post_id):
     # сюда передается айди поста который мы просматриваем
-
+    post_id = ObjectId(post_id)
     post_info = posts_collection.aggregate([
         {
             '$lookup':
@@ -35,20 +36,20 @@ def get_post_view(post_id):
         },
         {
             '$match': {
-                "id": post_id
+                "_id": post_id
             }
         },
         # исключение полей
         {
             '$project': {
-                'id': 1,
                 'text': 1,
+                'subject': 1,
                 # 'rating': 1,
                 'img': 1,
                 'author.username': 1,
                 'author.img': 1,
                 'author._id': 1,
-                '_id': 0,
+                '_id': 1,
                 'rating': {'result': 1},
                 'tags.tag_name': 1
             }
@@ -57,7 +58,13 @@ def get_post_view(post_id):
 
     post = [p for p in post_info]
 
+    post[0]['id'] = str(post[0]['_id'])
+    del post[0]['_id']
+    post[0]['author']['id'] = str(post[0]['author']['_id'])
+    del post[0]['author']['_id']
+
     response = {
         'post_info': post[0]
     }
+    print(post[0])
     return response
